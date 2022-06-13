c5d688e8423c0cb13324590e5dc0ef1abadda77b
Deepanshu Agarwal <78287659+Deepanshuagarwal1409@users.noreply.github.com>
2022-06-13 10:50:28 +0530

    Update CustomerController.php

--- laravel-rest-api-master/app/Http/Controllers/CustomerController.php
+++ laravel-rest-api-master/app/Http/Controllers/CustomerController.php
@@ -7,23 +7,14 @@ use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+   
     public function index()
     {
         $customers = Customer::orderBy('id', 'desc')->get();
         return response()->json($customers);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+  
     public function store(Request $request)
     {
         $customer = new Customer;
@@ -34,25 +25,13 @@ class CustomerController extends Controller
         return response()->json($request);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $customer = Customer::findOrFail($id);
         return response()->json($customer);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function update(Request $request, $id)
     {
         $customer = Customer::findOrFail($id);
@@ -63,12 +42,7 @@ class CustomerController extends Controller
         return response()->json($request);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function destroy($id)
     {
         $customer = Customer::findOrFail($id);
