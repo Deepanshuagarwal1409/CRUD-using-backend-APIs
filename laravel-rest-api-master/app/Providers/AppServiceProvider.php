d25bee7479a7593eb58a04a3ffc1067ea357828e
Deepanshu Agarwal <78287659+Deepanshuagarwal1409@users.noreply.github.com>
2022-06-13 10:53:37 +0530

    Update AppServiceProvider.php

--- laravel-rest-api-master/app/Providers/AppServiceProvider.php
+++ laravel-rest-api-master/app/Providers/AppServiceProvider.php
@@ -7,21 +7,13 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
+   
     public function register()
     {
         Schema::defaultStringLength(191);
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
+   
     public function boot()
     {
         //
